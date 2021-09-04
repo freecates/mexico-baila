ca649ccbd4542061b72faad9e0b9f490f00544df
Ramon Gil <ramon.gil@deporvillage.com>
2021-09-04 20:00:05 +0200

    with-latest-nextjs [mexico-baila$] Upgraded to latest next js

--- next.config.js
+++ next.config.js
@@ -1,53 +1,23 @@
-const withOffline = moduleExists('next-offline') ? require('next-offline') : {}
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
-const nextConfig = {
-  workboxOpts: {
-    swDest: 'static/service-worker.js',
-    clientsClaim: true,
-    skipWaiting: true,
-    runtimeCaching: [
-      {
-        urlPattern: new RegExp('^https://mexicobailadata.now.sh/static/(.*)'),
-        handler: 'NetworkOnly'
-      },
-      {
-        urlPattern: /^https?.*/,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'https-calls',
-          expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
-          },
-          cacheableResponse: {
-            statuses: [0, 200]
-          }
-        }
-      }
-    ]
-  },
-  env: {
-    YOUTUBE_APY_KEY: process.env.YOUTUBE_APY_KEY,
-  },
-  target: 'serverless',
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
-    }
+module.exports = withPWA({
+    pwa: {
+        dest: 'public',
+        runtimeCaching,
+        disable: process.env.NODE_ENV === 'development',
+    },
+    env: {
+        YOUTUBE_APY_KEY: process.env.YOUTUBE_APY_KEY,
+    },
+    webpack5: false,
+    target: 'serverless',
+    webpack: (config) => {
+        // Fixes npm packages that depend on `fs` module
+        config.node = {
+            fs: 'empty',
+        };
 
-    return config
-  }
-}
-
-module.exports = moduleExists('next-offline')
-  ? withOffline(nextConfig)
-  : nextConfig
-
-function moduleExists(name) {
-  try {
-    return require.resolve(name)
-  } catch (error) {
-    return false
-  }
-}
+        return config;
+    },
+});
--- pages/el-grupo.js
+++ pages/el-grupo.js
@@ -1,26 +1,26 @@
-import fetch from 'isomorphic-unfetch'
-import Head from 'next/head'
-import React from 'react'
-import Logo from '../components/Logo'
-import MainLayout from '../components/MainLayout'
-import ResponsiveParallax from '../components/ResponsiveParallax'
-import BasicPageStyles from '../components/styles/BasicPageStyles'
+import fetch from 'isomorphic-unfetch';
+import Head from 'next/head';
+import React from 'react';
+import Logo from '../components/Logo';
+import MainLayout from '../components/MainLayout';
+import ResponsiveParallax from '../components/ResponsiveParallax';
+import BasicPageStyles from '../components/styles/BasicPageStyles';
 
-const ElGrupo = props => (
-  <MainLayout mainlayout>
-    <Head>
-      <title>{props.data.subtitle} | México Baila</title>
-      <meta name="description" content={props.data.content.lead} />
+const ElGrupo = ({ data }) => (
+    <MainLayout mainlayout>
+        <Head>
+            <title>{data.subtitle} | México Baila</title>
+            <meta name='description' content={data.content.lead} />
 
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{
-          __html: `
+            <script
+                type='application/ld+json'
+                dangerouslySetInnerHTML={{
+                    __html: `
           {
             "@context": "http://schema.org",
             "@type": "WebPAge",
-            "name": "${props.data.subtitle}",
-            "description": "${props.data.content.lead}",
+            "name": "${data.subtitle}",
+            "description": "${data.content.lead}",
             "author": {
               "@type": "Person",
               "name": "Ramon Gil"
@@ -33,81 +33,83 @@ const ElGrupo = props => (
               "url": "https://mexicobaila.com/static/icons/android-chrome-512x512.png"
                 }
               }, 
-            "image": "${props.data.images[0].src}"
-          }`
-        }}
-      />
-    </Head>
+            "image": "${data.images[0].src}"
+          }`,
+                }}
+            />
+        </Head>
 
-    <BasicPageStyles>
-      <Logo />
+        <BasicPageStyles>
+            <Logo />
 
-      <div className="hero no-height black">
-        <h1 className="title">{props.data.title}</h1>
-        <h2 className="subtitle">{props.data.subtitle}</h2>
-      </div>
-      <div className="hero with-img">
-        <ResponsiveParallax
-          bgImage={props.data.images[0].src}
-          bgImageAlt={props.data.images[0].alt}
-          bgImageSrcSet={props.data.images[0].srcSet}
-          bgImageSizes="(max-width: 480px) 480px, (max-width: 800px) 800px, 4032px"
-        />
-      </div>
+            <div className='hero no-height black'>
+                <h1 className='title'>{data.title}</h1>
+                <h2 className='subtitle'>{data.subtitle}</h2>
+            </div>
+            <div className='hero with-img'>
+                <ResponsiveParallax
+                    bgImage={data.images[0].src}
+                    bgImageAlt={data.images[0].alt}
+                    bgImageSrcSet={data.images[0].srcSet}
+                    bgImageSizes='(max-width: 480px) 480px, (max-width: 800px) 800px, 4032px'
+                />
+            </div>
 
-      <div className="hero no-height">
-        <p className="subtitle">{props.data.content.lead}</p>
+            <div className='hero no-height'>
+                <p className='subtitle'>{data.content.lead}</p>
 
-        <div className="row sm-12 md-6">
-          <p className="card align-left">{props.data.content.first}</p>
+                <div className='row sm-12 md-6'>
+                    <p className='card align-left'>{data.content.first}</p>
 
-          <p className="card align-left">{props.data.content.second}</p>
-        </div>
-      </div>
-      <div className="hero with-img black">
-        <ResponsiveParallax
-          bgImage={props.data.images[1].src}
-          bgImageAlt={props.data.images[1].alt}
-          bgImageSrcSet={props.data.images[1].srcSet}
-          bgImageSizes="(max-width: 480px) 480px, (max-width: 800px) 800px, 4048px"
-        />
-      </div>
+                    <p className='card align-left'>{data.content.second}</p>
+                </div>
+            </div>
+            <div className='hero with-img black'>
+                <ResponsiveParallax
+                    bgImage={data.images[1].src}
+                    bgImageAlt={data.images[1].alt}
+                    bgImageSrcSet={data.images[1].srcSet}
+                    bgImageSizes='(max-width: 480px) 480px, (max-width: 800px) 800px, 4048px'
+                />
+            </div>
 
-      <div className="hero no-height black">
-        <h1 className="title">{props.data.bio.title}</h1>
-        <p className="description">{props.data.bio.subtitle}</p>
-        <p
-          className="description"
-          dangerouslySetInnerHTML={{
-            __html: props.data.bio.lead
-          }}
-        />
+            <div className='hero no-height black'>
+                <h1 className='title'>{data.bio.title}</h1>
+                <p className='description'>{data.bio.subtitle}</p>
+                <p
+                    className='description'
+                    dangerouslySetInnerHTML={{
+                        __html: data.bio.lead,
+                    }}
+                />
 
-        <div className="row sm-12 md-6">
-          <p
-            className="card align-left"
-            dangerouslySetInnerHTML={{
-              __html: props.data.bio.first
-            }}
-          />
+                <div className='row sm-12 md-6'>
+                    <p
+                        className='card align-left'
+                        dangerouslySetInnerHTML={{
+                            __html: data.bio.first,
+                        }}
+                    />
 
-          <p
-            className="card align-left"
-            dangerouslySetInnerHTML={{
-              __html: props.data.bio.second
-            }}
-          />
-        </div>
-      </div>
-    </BasicPageStyles>
-  </MainLayout>
-)
+                    <p
+                        className='card align-left'
+                        dangerouslySetInnerHTML={{
+                            __html: data.bio.second,
+                        }}
+                    />
+                </div>
+            </div>
+        </BasicPageStyles>
+    </MainLayout>
+);
 
-ElGrupo.getInitialProps = async function() {
-  const res = await fetch(`https://mexicobailadata.now.sh/el-grupo.json`)
-  const data = await res.json()
+export const getStaticProps = async () => {
+    const res = await fetch(`https://mexicobailadata.now.sh/el-grupo.json`);
+    const data = await res.json();
+    return {
+        props: { data },
+        revalidate: 1,
+    };
+};
 
-  return { data }
-}
-
-export default ElGrupo
+export default ElGrupo;
