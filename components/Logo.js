21510ff83a850a04a927e6b3301342e402d52b9a
Ramon Gil <ramon.gil@deporvillage.com>
2021-09-04 20:17:27 +0200

    width and height for logo

--- components/Logo.js
+++ components/Logo.js
@@ -1,14 +1,19 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
 const Logo = () => (
-  <header className="description black">
-    <Link href="/">
-      <a title="Inicio">
-        <img src="/static/logo-mexico-baila.png" alt="Logo Mexico Baila" />
-      </a>
-    </Link>
-  </header>
-)
+    <header className='description black'>
+        <Link href='/'>
+            <a title='Inicio'>
+                <img
+                    src='/static/logo-mexico-baila.png'
+                    alt='Logo Mexico Baila'
+                    width='200'
+                    height='200'
+                />
+            </a>
+        </Link>
+    </header>
+);
 
-export default Logo
+export default Logo;
--- components/nav.js
+++ components/nav.js
@@ -1,40 +1,42 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
 const links = [
-  { href: '/contacto', label: 'Contacto' },
-  { href: '/el-grupo', label: 'El Grupo' },
-  { href: '/servicios', label: 'Servicios' },
-  { href: '/videos', label: 'Vídeos' }
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
-})
+    { href: '/contacto', label: 'Contacto' },
+    { href: '/el-grupo', label: 'El Grupo' },
+    { href: '/servicios', label: 'Servicios' },
+    { href: '/videos', label: 'Vídeos' },
+].map((link) => {
+    link.key = `nav-link-${link.href}-${link.label}`;
+    return link;
+});
 
-const Nav = props => (
-  <nav>
-    <ul>
-      {props.withHome && (
-        <li>
-          <Link href="/">
-            <a title="Inicio">
-              <img
-                src="/static/logo-mexico-baila-footer.png"
-                alt="Logo Mexico Baila"
-              />
-            </a>
-          </Link>
-        </li>
-      )}
-      {links.map(({ key, href, label }) => (
-        <li key={key}>
-          <Link href={href}>
-            <a>{label}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </nav>
-)
+const Nav = (props) => (
+    <nav>
+        <ul>
+            {props.withHome && (
+                <li>
+                    <Link href='/'>
+                        <a title='Inicio'>
+                            <img
+                                src='/static/logo-mexico-baila-footer.png'
+                                alt='Logo Mexico Baila'
+                                width='75'
+                                height='75'
+                            />
+                        </a>
+                    </Link>
+                </li>
+            )}
+            {links.map(({ key, href, label }) => (
+                <li key={key}>
+                    <Link href={href}>
+                        <a>{label}</a>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    </nav>
+);
 
-export default Nav
+export default Nav;
